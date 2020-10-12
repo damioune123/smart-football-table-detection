f784b3c46629c948e2471a565c800c1e0355295c
dmeur <damienmeur@gmail.com>
2020-10-12 02:04:20 +0200

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 [ -z "$MODEL_PATH" ] && MODEL_PATH=/darknet/yolov3-models/modelFromCOM19/files/
-cd $MODEL_PATH && python -u /darknet/darknet_video.py -s "$MQTTHOST" -p "$MQTTPORT"
+cd $MODEL_PATH && python3 -u /darknet/darknet_video.py -s "$MQTTHOST" -p "$MQTTPORT"
 
